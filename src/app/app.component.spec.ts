a9ea29aa9b997088b8173772d612772a9f65b210
Ethay Tam <ethay012@gmail.com>
2022-08-26 21:45:30 +0300

    feat: added testing-library eslint plugin

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -4,7 +4,7 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   test('displays a welcome message', async () => {
     await render(AppComponent);
-    const welcome = screen.getByText('Welcome')
+    const welcome = screen.getByText('Welcome');
     expect(welcome).toBeTruthy();
-  })
+  });
 });
