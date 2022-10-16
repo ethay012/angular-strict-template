e4ebf799ad6bb70b980da2ef94f20d83138fe23d
Ethay Tam <ethay012@gmail.com>
2022-10-17 00:12:26 +0300

    feat: changed default schematic to SCAM pattern

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
 
 @Component({
   selector: 'app-root',
@@ -8,3 +10,11 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'angular-strict-template';
 }
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
